2ddeeb28093fc54f92ce89eefa73757e5c8083fe
Wentian Windows PC <wbai1295@gmail.com>
2019-01-23 10:59:39 -0500

    new project1

---
+++ Projects/Project1/Project1/Project1.cpp
@@ -0,0 +1,13 @@
+// Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Process.h"
+
+#ifdef TEST_TESTPROJECT
+int main()
+{
+	
+    std::cout << "Hello World!\n"; 
+}
+#endif
