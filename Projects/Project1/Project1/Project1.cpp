5533a7f31fea5ddc88bef1165bb69172c928e217
Wentian Windows PC <wbai1295@gmail.com>
2019-01-23 20:55:52 -0500

    add my process

--- Projects/Project1/Project1/Project1.cpp
+++ Projects/Project1/Project1/Project1.cpp
@@ -7,7 +7,28 @@
 #ifdef TEST_TESTPROJECT
 int main()
 {
-	
-    std::cout << "Hello World!\n"; 
+	Process p;
+	std::string appPath =
+		//"c:/windows/explorer.exe";  // path to application to start
+		"c:/Program Files (x86)/Google/Chrome/Application/chrome.exe";
+	p.application(appPath);
+
+	std::cout << "\n open test.html 2 times\n";
+	getchar();
+
+	for (int i = 0; i < 2; i++) {
+		std::string cmdLine = "/A ../Project1/test" + std::to_string(i) + ".html";
+		p.commandLine(cmdLine);
+
+		std::cout << "\n starting process\n";
+		p.create();
+		CBP callback = []() {};
+		p.setCallBackProcessing(callback);
+		p.registerCallback();
+
+		WaitForSingleObject(p.getProcessHandle(), INFINITE);
+	}
+
+	return 0;
 }
 #endif
